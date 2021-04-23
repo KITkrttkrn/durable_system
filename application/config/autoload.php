44a586dff53737e1ed54dd1fef8ab1544e044e8a
Krittikarn Verakalas <vkrittikarn@gmail.com>
2021-04-23 17:07:54 +0700

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |    $autoload['model'] = array('first_model' => 'first');
  */
-$autoload['model'] = array('sysconfig');
+$autoload['model'] = array('sysconfig','login_model');
